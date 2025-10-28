d191ce419effc08db9d0d14953e9ebb397a81f81
Suchan Badyakar <suchan211@gmail.com>
2025-10-28 11:17:48 +1100

    Update example files

--- example/app.js
+++ example/app.js
@@ -1,36 +1,74 @@
-// This is a test harness for your module
-// You should do something interesting in this harness
-// to test out the module and to provide instructions
-// to users on how to use it by example.
+// Example usage of the TiGainsight module
+const Gainsight = require('ti.gainsight');
 
+const win = Ti.UI.createWindow({
+    backgroundColor: '#ffffff',
+    layout: 'vertical'
+});
+const statusLabel = Ti.UI.createLabel({
+    text: 'Tap buttons to test GainsightPX',
+    textAlign: 'center',
+    color: '#333',
+    top: 50
+});
+const initButton = Ti.UI.createButton({
+    title: 'Initialize GainsightPX',
+    top: 100,
+    width: 200
+});
+const enableButton = Ti.UI.createButton({
+    title: 'Enable GainsightPX',
+    top: 20,
+    width: 200
+});
+const screenButton = Ti.UI.createButton({
+    title: 'Track Screen Event',
+    top: 20,
+    width: 200
+});
+const identifyButton = Ti.UI.createButton({
+    title: 'Identify User',
+    top: 20,
+    width: 200
+});
 
-// open a single window
-const win = Ti.UI.createWindow();
-const label = Ti.UI.createLabel();
-win.add(label);
+initButton.addEventListener('click', function() {
+    Gainsight.init('your-product-id', function(response) {
+        Ti.API.info('Gainsight initialized:', response);
+        statusLabel.text = 'GainsightPX initialized!';
+    });
+});
+
+enableButton.addEventListener('click', function() {
+    Gainsight.enable(function(response) {
+        Ti.API.info('Gainsight enabled:', response);
+        statusLabel.text = 'GainsightPX enabled!';
+    });
+});
+
+screenButton.addEventListener('click', function() {
+    Gainsight.screenEvent('ExampleScreen');
+    Ti.API.info('Screen event tracked');
+    statusLabel.text = 'Screen event tracked';
+});
+
+identifyButton.addEventListener('click', function() {
+    Gainsight.identify({
+        userId: 'user123',
+        email: 'user@example.com',
+        firstName: 'John',
+        lastName: 'Doe',
+        accountId: 'account123',
+        accountName: 'Acme Inc.'
+    }, function(response) {
+        Ti.API.info('User identified:', response);
+        statusLabel.text = 'User identified';
+    });
+});
+
+win.add(statusLabel);
+win.add(initButton);
+win.add(enableButton);
+win.add(screenButton);
+win.add(identifyButton);
 win.open();
-
-// TODO: write your module tests here
-import ti_gainsight  from 'ti.gainsight';
-Ti.API.info("module is => " + ti_gainsight);
-
-label.text = ti_gainsight.example();
-
-Ti.API.info("module exampleProp is => " + ti_gainsight.exampleProp);
-ti_gainsight.exampleProp = "This is a test value";
-
-if (Ti.Platform.name == "android") {
-	const proxy = ti_gainsight.createExample({
-		message: "Creating an example Proxy",
-		backgroundColor: "red",
-		width: 100,
-		height: 100,
-		top: 100,
-		left: 150
-	});
-
-	proxy.printMessage("Hello world!");
-	proxy.message = "Hi world!.  It's me again.";
-	proxy.printMessage("Hello world!");
-	win.add(proxy);
-}
